7c33ee5e0bdf7b823055c2cf5fbbfa0c7dbf4dee
Devin Lehmacher <lehmacdj@gmail.com>
2022-03-25 10:27:54 -0700

    format

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,11 @@
 module Main where
 
 import Prelude
-
 import Effect (Effect)
-import Specular.Dom.Element (attr,  class_, dynText, el, onClick_, text)
+import Specular.Dom.Element (attr, class_, dynText, el, onClick_, text)
 import Specular.Dom.Widget (runMainWidgetInBody)
 import Specular.Ref (Ref)
 import Specular.Ref as Ref
-
-
 
 main :: Effect Unit
 main = do
@@ -16,15 +13,13 @@ main = do
   runMainWidgetInBody do
     counter :: Ref Int <- Ref.new 0
     -- | Subtract 1 from counter value
-    let subtractCb = Ref.modify counter (add (negate 1))
-
+    let
+      subtractCb = Ref.modify counter (add (negate 1))
     -- | Add 1 to counter value
-    let addCb = Ref.modify counter (add 1)
-
-    el "button" [class_ "btn", attr "type" "button", onClick_ addCb ] do
+    let
+      addCb = Ref.modify counter (add 1)
+    el "button" [ class_ "btn", attr "type" "button", onClick_ addCb ] do
       text "+"
-
     dynText $ show <$> Ref.value counter
-
-    el "button" [class_ "btn", attr "type" "button", onClick_ subtractCb ] do
+    el "button" [ class_ "btn", attr "type" "button", onClick_ subtractCb ] do
       text "-"
